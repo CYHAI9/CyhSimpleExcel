b13863e60f901788c88b19a8c2900b603e326348
又是一个大叔 <3069126500@qq.com>
2017-10-17 17:58:34 +0800

    no message

--- CyhSimpleExcel/CyhSimpleExcel/ViewController.m
+++ CyhSimpleExcel/CyhSimpleExcel/ViewController.m
@@ -16,14 +16,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"Excel" ofType:@"plist"];
     NSDictionary * dataSource = [[NSDictionary alloc] initWithContentsOfFile:filePath];
-    //    NSLog(@"plist文件数据:\n%@",dataSource[@"data"]);
+        NSLog(@"plist文件数据:\n%@",dataSource);
     CyhExcelSimple * excelView = [[CyhExcelSimple alloc] initWithFrame:CGRectMake(0, 90, self.view.frame.size.width, 200)];
     [self.view addSubview:excelView];
     NSArray * excelSource = dataSource[@"data"];
     NSDictionary * excel01Data = excelSource[0];
     [excelView setExcelTitle:excel01Data[@"ExcelTitle"] excelListTitle:excel01Data[@"ExcelListTitle"] excelValueTitle:excel01Data[@"ExcelValueTitle"] excelValues:excel01Data[@"ExcelListValues"]];
+    
     [excelView setExcelType:^(UILabel *ExcelTileLabel, UILabel *listTitleLabel, UILabel *valuesTitleLabel, UILabel *ValuesLable) {
         ExcelTileLabel.backgroundColor = [UIColor orangeColor];
         listTitleLabel.backgroundColor = [UIColor blueColor];
